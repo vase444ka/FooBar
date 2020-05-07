633b0c46504759b60b165bcc8f6576ac67524ac0
Julia <hedviga258@gmail.com>
2020-05-08 00:04:20 +0300

    Grid

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QPushButton>
+#include "grid.h"
 
 
 
@@ -9,12 +10,41 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
-    for(auto ch : ui->centralwidget->children())
-    {
-        qInfo() << ch->objectName();
-        connect(ch, SIGNAL(clicked(bool)), this, SLOT(clikkk(bool)));
-    }
+    this->resize(700, 800);
+    this->setWindowTitle(tr("ШАХМАТЫ ХУЛЕ"));
+    QGridLayout *grid = new QGridLayout(ui->centralwidget);
+    grid->setSizeConstraint(QLayout::SetFixedSize);
+    //grid->setGeometry(QRect(0,0,500,500));
+
+    QPalette *green = new QPalette();
+    green->setColor(QPalette::Background, Qt::green);
+
+    QPalette *red = new QPalette();
+    red->setColor(QPalette::Background, Qt::red);
+    for (int i = 0; i < 8; ++i)
+        for (int j = 0; j < 8; ++j)
+        {
+            QPushButton *w = new QPushButton();
+            //w->setFlat(true);
+            w->setFixedSize(70, 70);
+            //w->setAutoFillBackground(true);
+            if ((i+j)%2)
+                w->setStyleSheet(tr("background-color: red;"));
+            else
+                w->setStyleSheet(tr("background-color: green;"));
+
+            //w->setAutoDefault(true);
+            //w->setDefault(true);
+
+            grid->addWidget(w, i, j);
+        }
+
+
+
+
+
 }
 
 MainWindow::~MainWindow()
